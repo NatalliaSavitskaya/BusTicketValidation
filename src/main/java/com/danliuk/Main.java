87ff83bb4678a426852d4d69b0efa6b11392d4b0
Natallia Savitskaya <nn.savitskaya@gmail.com>
2024-07-31 15:35:11 +0300

    error with "" is fixed; Bus Ticket Validation with information messages in console added

--- src/main/java/com/danliuk/Main.java
+++ src/main/java/com/danliuk/Main.java
@@ -8,19 +8,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
-
+        int counterValidTickets = 0;
+        int counterValidPrice = 0;
+        int counterValidStartDate = 0;
+        int counterTotalTickets = 5;
         int x = 0;
 
         do {
             String input = getInput();
             BusTicket busTicket = new ObjectMapper().readValue(input, BusTicket.class);
 
-            // TODO: ticket validation
+            // Bus Ticket validation
+               if (busTicket.isValidPrice())
+                   counterValidPrice++;
+               else  System.out.println("Invalid price.");
+               if (busTicket.isValidStartDate())
+                   counterValidStartDate++;
+               else  System.out.println("Invalid start date of specific ticket types.");
+               if (busTicket.isValidPrice() && busTicket.isValidStartDate())
+                   counterValidTickets++;
 
             System.out.println(busTicket.toString());
             x++;
 
-        } while (x < 5);
+        } while (x < counterTotalTickets);
+
+        System.out.println("Total = " + x++);
+        System.out.println("Valid = " + counterValidTickets++);
+        if (counterValidPrice > counterValidStartDate)
+            System.out.println("Most popular violation = price.");
+        else if (counterValidPrice < counterValidStartDate)
+                System.out.println("Most popular violation = start date.");
+        else System.out.println("Most popular violation = start date and price.");
     }
 
     private static String getInput() {
--- src/main/java/com/danliuk/model/BusTicket.java
+++ src/main/java/com/danliuk/model/BusTicket.java
@@ -7,13 +7,25 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+
 public class BusTicket {
-
     private String ticketClass;
-
     private String ticketType;
+    private String startDate; // DAY, WEEK, YEAR
+    private String price; // !=zero
 
-    private String startDate;
+    // constructors
+    public BusTicket() { }
 
-    private String price;
+    public boolean isValidPrice() {
+        if (price == null) return false;
+        else
+            return (Double.parseDouble(price) != 0.0);
+    }
+
+    public boolean isValidStartDate() {
+        if ("DAY".equals(ticketType) || "WEEK".equals(ticketType) || "YEAR".equals(ticketType))
+            return (startDate != null)&&(!startDate.isEmpty());
+        else return true;
+    }
 }
