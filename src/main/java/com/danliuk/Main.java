93dbdce8885bfd85419a65653613ba8b4baffa8b
Natallia Savitskaya <nn.savitskaya@gmail.com>
2024-07-31 17:50:30 +0300

    new validation ticket rules applied

--- src/main/java/com/danliuk/Main.java
+++ src/main/java/com/danliuk/Main.java
@@ -3,15 +3,17 @@ package com.danliuk;
 import com.danliuk.model.BusTicket;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.Math;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
-        int counterValidTickets = 0;
-        int counterValidPrice = 0;
-        int counterValidStartDate = 0;
         int counterTotalTickets = 5;
+        int counterValidTickets = 0;
+        int counterInalidTicketType = 0;
+        int counterInalidStartDate = 0;
+        int counterInvalidPrice = 0;
         int x = 0;
 
         do {
@@ -19,13 +21,19 @@ public class Main {
             BusTicket busTicket = new ObjectMapper().readValue(input, BusTicket.class);
 
             // Bus Ticket validation
-               if (busTicket.isValidPrice())
-                   counterValidPrice++;
-               else  System.out.println("Invalid price.");
-               if (busTicket.isValidStartDate())
-                   counterValidStartDate++;
-               else  System.out.println("Invalid start date of specific ticket types.");
-               if (busTicket.isValidPrice() && busTicket.isValidStartDate())
+            if (!busTicket.isValidTicketType()) {
+                counterInalidTicketType++;
+                System.out.println("Invalid ticket type.");
+            }
+            if (!busTicket.isValidStartDate()) {
+                counterInalidStartDate++;
+                System.out.println("Invalid start date.");
+            }
+            if (!busTicket.isValidPrice()) {
+                counterInvalidPrice++;
+                System.out.println("Invalid price.");
+            }
+            if (busTicket.isValidTicketType() && busTicket.isValidStartDate() && busTicket.isValidPrice())
                    counterValidTickets++;
 
             System.out.println(busTicket.toString());
@@ -35,11 +43,15 @@ public class Main {
 
         System.out.println("Total = " + x++);
         System.out.println("Valid = " + counterValidTickets++);
-        if (counterValidPrice > counterValidStartDate)
-            System.out.println("Most popular violation = price.");
-        else if (counterValidPrice < counterValidStartDate)
-                System.out.println("Most popular violation = start date.");
-        else System.out.println("Most popular violation = start date and price.");
+
+        int max = Math.max(counterInalidTicketType, Math.max(counterInalidStartDate, counterInvalidPrice));
+        if (max == 0)
+            System.out.println("There is no invalid tickets.");
+        else if (max==counterInalidTicketType)
+                 System.out.println("Most popular violation = type.");
+             else if (max==counterInalidStartDate)
+                     System.out.println("Most popular violation = start date.");
+                  else System.out.println("Most popular violation = price.");
     }
 
     private static String getInput() {
--- src/main/java/com/danliuk/model/BusTicket.java
+++ src/main/java/com/danliuk/model/BusTicket.java
@@ -3,6 +3,9 @@ package com.danliuk.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Getter
 @Setter
@@ -10,22 +13,29 @@ import lombok.ToString;
 
 public class BusTicket {
     private String ticketClass;
-    private String ticketType;
-    private String startDate; // DAY, WEEK, YEAR
-    private String price; // !=zero
+    private String ticketType; // DAY, WEEK, YEAR, MONTH.
+    private String startDate; // can't be in the future. For [DAY, WEEK, YEAR] ticketTypes shouldn't be empty
+    private String price; // should always be even
 
-    // constructors
+    // constructor
     public BusTicket() { }
 
-    public boolean isValidPrice() {
-        if (price == null) return false;
-        else
-            return (Double.parseDouble(price) != 0.0);
+        public boolean isValidTicketType() {
+        return "DAY".equals(ticketType) || "WEEK".equals(ticketType) || "YEAR".equals(ticketType) || "MONTH".equals(ticketType);
     }
 
     public boolean isValidStartDate() {
-        if ("DAY".equals(ticketType) || "WEEK".equals(ticketType) || "YEAR".equals(ticketType))
-            return (startDate != null)&&(!startDate.isEmpty());
-        else return true;
+       if ((startDate == null)||(startDate.isEmpty()))
+           return !"DAY".equals(ticketType) && !"WEEK".equals(ticketType) && !"YEAR".equals(ticketType);
+       else {
+           LocalDate date = LocalDate.parse(startDate, DateTimeFormatter.ISO_LOCAL_DATE);
+           return (!date.isAfter(LocalDate.now()));
+       }
+    }
+
+    public boolean isValidPrice() {
+        if ((price == null)||(price.isEmpty())) return false;
+        else
+            return (Double.parseDouble(price) % 2 == 0.0);
     }
 }
